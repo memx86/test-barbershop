eddbfed9052a65a86c47da02cb2d4f08608cc40a
Yevhen Malyshko <memx86@gmail.com>
2021-12-29 15:25:40 +0200

    linked navigation with sections

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,21 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-  const mobileLinks = document.querySelectorAll('.mobile-menu__link');
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileLinks = document.querySelectorAll("[data-mobile-link]");
 
   const doMagic = () => {
     const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    mobileMenuRef.classList.toggle('is-open');
-    document.body.classList.toggle('modal-open');
+    mobileMenuRef.classList.toggle("is-open");
+    document.body.classList.toggle("modal-open");
   };
 
-  menuBtnRef.addEventListener('click', doMagic);
+  menuBtnRef.addEventListener("click", doMagic);
   for (const link of mobileLinks) {
-    link.addEventListener('click', doMagic);
+    link.addEventListener("click", doMagic);
   }
 })();
